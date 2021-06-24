f87b2349f3594fbb52ab385691d3961910fb6a67
Alejandro Muniz <alejandro.muniz@yahoo.com>
2021-06-24 06:17:08 +0000

    Added "try catch" to check if y == 0

--- main.cs
+++ main.cs
@@ -4,19 +4,39 @@ using System.Linq;
 
 class MainClass 
 {
-static int Divide (int x, int y)
-{
-  return x / y;
-}
+  static int Divide (int x, int y)
+  {
+    return x / y;
+  }
+
+
+
 
   public static void Main (string[] args) 
   {
     Console.WriteLine ("Hello World");
-    
+    Console.WriteLine ("\n");
+
     int x = 567;
-    int y = 6;
+    int y = 0;
+    Console.WriteLine ($"Your x value is {x} and your y value is {y}");
+
+    try 
+    {
+      if (y == 0) throw new ArgumentOutOfRangeException("y cannot equal zero");
+    }
     
-    Console.WriteLine(Divide (x,y));
+    catch (ArgumentOutOfRangeException e) 
+    {      
+      Console.Write($"Enter your y value: {y = 6} \n");
+      //if you want user input add: y = Int32.Parse(Console.ReadLine());
+      Console.WriteLine(e);      
+    }
+    finally
+    {
+      Console.WriteLine($"\n The score is {Divide (x,y)}.\n");
+    }
+    
 
   }
 }
