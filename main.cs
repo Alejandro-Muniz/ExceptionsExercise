c922810d3af15c974965bb5ffcdcf181aea1c3ad
Alejandro Muniz <alejandro.muniz@yahoo.com>
2021-06-24 05:24:55 +0000

    Created a method to handle the division of x and y.  Called the method from main.  Commit'ing code to have a working revert point.

--- main.cs
+++ main.cs
@@ -1,7 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class MainClass {
-  public static void Main (string[] args) {
+class MainClass 
+{
+static int Divide (int x, int y)
+{
+  return x / y;
+}
+
+  public static void Main (string[] args) 
+  {
     Console.WriteLine ("Hello World");
+    
+    int x = 567;
+    int y = 6;
+    
+    Console.WriteLine(Divide (x,y));
+
   }
 }
